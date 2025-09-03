e21fc28b0c667b63b62fb43572906c4bf262571d
DjJazzyJeff2425 <jaz2185345@maricopa.edu>
2025-09-02 20:41:49 -0700

    added hello world

--- src/main/java/org/example/sandbox/Hello.java
+++ src/main/java/org/example/sandbox/Hello.java
@@ -1,10 +1,7 @@
 package org.example.sandbox;
-
 public class Hello {
-
-    public static void main(String[] args) {
-
-        Hello hello = new Hello();
+	public static void main(String[] args)
+	{
         System.out.println("Hello, World!");
     }
 
